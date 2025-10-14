f455ea649610ec76d343746c0701f5416c81f629
Ecj Taneo <ecjtaneo@gmail.com>
2025-10-14 11:01:03 +0800

    Added comments

--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,14 @@ import express, { type NextFunction, type Request, type Response } from 'express
 import passport from 'passport'
 import './config/passport.js'
 import crypto from 'crypto'
+//import jwt from'jsonwebtoken'
 
 const app = express()
 
 function isLoggedIn(req: Request, res: Response, next: NextFunction) {
     //Check if JWT is VALID
-    //Or Check if REQ.USER EXISTS
+    //Or Check if REQ.USER EXISTS --- if using SESSIONS
+    // But we use JWT here so. Check JWTs.
     console.log("REQ.USER: ", req.user)
     if(!req.user) { return res.sendStatus(401) }
     next()
@@ -26,7 +28,7 @@ app.get('/auth/callback', passport.authenticate(
 
     //simulate jwts sending after success login
     const user = req.user as any
-    const token = crypto.createHash('sha256').update('username').digest('hex')
+    const token = crypto.createHash('sha256').update('test').digest('hex')
     console.log(`${user.displayName} Successfully logged in with Token: ${token}`)
     res.json({token})
 
@@ -35,10 +37,11 @@ app.get('/auth/callback', passport.authenticate(
 
 app.get("/auth/fail", (_, res) => res.sendStatus(400));
 
-app.get('/protectedtest', isLoggedIn,(req, res) => {
-    console.log("User passed!", req.user)
-    res.send("Hello!")
-})
+
+//app.get('/protectedtest', isLoggedIn,(req, res) => {
+    //console.log("User passed!", req.user)
+    //res.send("Hello!")
+//})
 
 
 //error handler middleware
